cf83aa04612c070a74c49ae58b7b74a3cc2de6fe
ebk13579 <evan.kozierachi@gmail.com>
2019-01-08 13:31:10 -0800

    1st burger

---
+++ db/burgerseeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Kahuna Burger');
+INSERT INTO burgers (burger_name) VALUES ('Krusty Burger');
+INSERT INTO burgers (burger_name) VALUES ('White Castle Slider');
+INSERT INTO burgers (burger_name) VALUES ('Double Double');
